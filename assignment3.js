c7795c8303bc46a29eeec04ad117bb5ba61eb029
Khushi-agarwal-2125 <khushiagarwal2125@gmail.com>
2025-05-19 23:11:09 -0700

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,9 @@
+function bw() {
+    // window.alert("Color Change");
+    document.getElementById("i1").src="sunrise image.jpg";
+}
+
+function clr() {
+    // alert();
+    document.getElementById("i1").src="sunrise image.jpg";
+}
---
+++ obj.js
@@ -0,0 +1,5 @@
+const obj = { name: "khushi", age: 30, };
+const obj2 = { name: "harsh", age: 30, };
+console.log(obj1.name);
+console.log(obj.name);
+console.log(obj2.name);
